56f5c463a5725f686cc9a1b9063d7c77c73c9002
rusdacent <rusdacentx0x08@gmail.com>
2021-07-06 20:16:54 +0300

    rm waiting key for windows build

--- build_windows.bat
+++ build_windows.bat
@@ -62,9 +62,3 @@ tar -a -cf %BUILD_DIR%\%TARGET%-%ARCH_BITS%bit.zip *
 rem Make the installer
 cd %PROJECT_DIR%
 %NSIS% /DNAME=%TARGET% /DARCH_BITS=%ARCH_BITS% installer_windows.nsi
-
-rem cls
-echo on
-@echo The resulting installer is %BUILD_DIR%\%TARGET%Setup-%ARCH_BITS%bit.exe.
-@echo Finished. Press any key to close this window.
-@timeout 10 > NUL
