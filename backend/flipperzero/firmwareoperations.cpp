8f3bf5f563ddb0dac75528b0ed4e427d0c246adc
Georgii Surkov <georgii.surkov@outlook.com>
2021-07-13 17:40:00 +0300

    Initial FUS upgrade implementation

--- backend/flipperzero/firmwareoperations.cpp
+++ backend/flipperzero/firmwareoperations.cpp
@@ -100,6 +100,13 @@ bool FUSDownloadOperation::execute()
         check_return_bool(m_device->detach(), "Failed to detach device");
     }
 
+    check_return_bool(m_device->setBootMode(FlipperZero::BootMode::DFUOnly), "Failed to set device into DFU-only boot mode");
+    check_return_bool(m_device->startFUS(), "Failed to start FUS");
+    check_return_bool(m_device->isFUSRunning(), "FUS seemed to start, but isn't running anyway");
+    check_return_bool(m_device->downloadWirelessStack(m_file, m_targetAddress), "Failed to download new FUS image");
+    check_return_bool(m_device->upgradeWirelessStack(), "Failed to upgrade FUS");
+    check_return_bool(m_device->setBootMode(FlipperZero::BootMode::Normal), "Failed to set device into Normal boot mode");
+
     m_device->setPersistent(false);
     return true;
 }
