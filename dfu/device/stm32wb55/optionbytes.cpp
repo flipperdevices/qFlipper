cd3955f65c876efe1fd844870c4588c9dce2a729
Georgii Surkov <georgii.surkov@outlook.com>
2021-07-28 14:39:58 +0300

    New option bytes class

--- dfu/device/stm32wb55/optionbytes.cpp
+++ dfu/device/stm32wb55/optionbytes.cpp
@@ -3,29 +3,17 @@
 
 #define OPTION_BYTES_SIZE 128
 
-#define nBOOT0_ADDR 0
-#define nBOOT0_POS 27
-
-#define nBOOT1_ADDR 0
-#define nBOOT1_POS 23
-
-#define nSWBOOT0_ADDR 0
-#define nSWBOOT0_POS 26
-
-#define SFSA_ADDR 0x70
-#define SFSA_POS 0
-
 namespace STM32 {
 namespace WB55 {
 
 OptionBytes::OptionBytes():
-    m_data(OPTION_BYTES_SIZE, 0),
-    m_isValid(false)
+    m_isValid(false),
+    m_data({})
 {}
 
 OptionBytes::OptionBytes(const QByteArray &data):
-    m_data(data),
-    m_isValid(data.size() == OPTION_BYTES_SIZE)
+    m_isValid(data.size() == OPTION_BYTES_SIZE),
+    m_data(*(OptionBytesData*)(data.data()))
 {}
 
 OptionBytes OptionBytes::invalid()
@@ -38,94 +26,213 @@ qint64 OptionBytes::size()
     return OPTION_BYTES_SIZE;
 }
 
-const QByteArray &OptionBytes::data() const
-{
-    return m_data;
-}
-
 bool OptionBytes::isValid() const
 {
     return m_isValid;
 }
 
+QByteArray OptionBytes::data() const
+{
+    return QByteArray::fromRawData((const char*)(&m_data), sizeof(OptionBytesData));
+}
+
 bool OptionBytes::nBoot0() const
 {
-    return getBit(nBOOT0_ADDR, nBOOT0_POS);
+    return m_data.word1[0].nBoot0;
 }
 
 bool OptionBytes::nBoot1() const
 {
-    return getBit(nBOOT1_ADDR, nBOOT1_POS);
+    return m_data.word1[0].nBoot1;
 }
 
 bool OptionBytes::nSwBoot0() const
 {
-    return getBit(nSWBOOT0_ADDR, nSWBOOT0_POS);
+    return m_data.word1[0].nSwBoot0;
+}
+
+bool OptionBytes::SRAM2PE() const
+{
+    return m_data.word1[0].SRAM2PE;
+}
+
+bool OptionBytes::SRAM2RST() const
+{
+    return m_data.word1[0].SRAM2RST;
+}
+
+uint8_t OptionBytes::AGCTRIM() const
+{
+    return m_data.word1[0].AGCTRIM;
+}
+
+bool OptionBytes::FSD() const
+{
+    return m_data.word9[0].FSD;
+}
+
+bool OptionBytes::DDS() const
+{
+    return m_data.word9[0].FSD;
+}
+
+bool OptionBytes::BRSD() const
+{
+    return m_data.word10[0].BRSD;
+}
+
+bool OptionBytes::NBRSD() const
+{
+    return m_data.word10[0].NBRSD;
+}
+
+bool OptionBytes::C2OPT() const
+{
+    return m_data.word10[0].C2OPT;
+}
+
+bool OptionBytes::PCROPRDP() const
+{
+    return m_data.word3[0].PCROPRDP;
+}
+
+uint8_t OptionBytes::RDP() const
+{
+    return m_data.word1[0].RDP;
+}
+
+bool OptionBytes::ESE() const
+{
+    return m_data.word1[0].ESE;
+}
+
+uint8_t OptionBytes::BORLEV() const
+{
+    return m_data.word1[0].BORLEV;
+}
+
+bool OptionBytes::nRSTSTOP() const
+{
+    return m_data.word1[0].nRSTSTOP;
+}
+
+bool OptionBytes::nRSTSTDBY() const
+{
+    return m_data.word1[0].nRSTSTDBY;
+}
+
+bool OptionBytes::nRSTSHDW() const
+{
+    return m_data.word1[0].nRSTSHDW;
+}
+
+bool OptionBytes::IWDGSW() const
+{
+    return m_data.word1[0].IWDGSW;
+}
+
+bool OptionBytes::IWDGSTOP() const
+{
+    return m_data.word1[0].IWDGSTOP;
+}
+
+bool OptionBytes::IWGDSTDBY() const
+{
+    return m_data.word1[0].IWGDSTDBY;
+}
+
+bool OptionBytes::WWDGSW() const
+{
+    return m_data.word1[0].WWDGSW;
 }
 
 uint8_t OptionBytes::SFSA() const
 {
-    return getByte(SFSA_ADDR, SFSA_POS);
+    return m_data.word9[0].SFSA;
+}
+
+uint8_t OptionBytes::SBRSA() const
+{
+    return m_data.word10[0].SBRSA;
+}
+
+uint8_t OptionBytes::SNBRSA() const
+{
+    return m_data.word10[0].SNBRSA;
+}
+
+uint16_t OptionBytes::PCROP1ASTRT() const
+{
+    return m_data.word2[0].PCROP1ASTRT;
+}
+
+uint16_t OptionBytes::PCROP1AEND() const
+{
+    return m_data.word3[0].PCROP1AEND;
+}
+
+uint16_t OptionBytes::PCROP1BSTRT() const
+{
+    return m_data.word6[0].PCROP1BSTRT;
+}
+
+uint16_t OptionBytes::PCROP1BEND() const
+{
+    return m_data.word7[0].PCROP1BEND;
+}
+
+uint8_t OptionBytes::WRP1ASTRT() const
+{
+    return m_data.word4[0].WRP1ASTRT;
+}
+
+uint8_t OptionBytes::WRP1AEND() const
+{
+    return m_data.word4[0].WRP1AEND;
+}
+
+uint8_t OptionBytes::WRP1BSTRT() const
+{
+    return m_data.word5[0].WRP1BSTRT;
+}
+
+uint8_t OptionBytes::WRP1BEND() const
+{
+    return m_data.word5[0].WRP1BEND;
+}
+
+uint16_t OptionBytes::IPCCDBA() const
+{
+    return m_data.word8[0].IPCCDBA;
+}
+
+uint32_t OptionBytes::SBRV() const
+{
+    return m_data.word10[0].SBRV;
 }
 
 void OptionBytes::setNBoot0(bool set)
 {
-    setBit(nBOOT0_ADDR, nBOOT0_POS, set);
+    m_data.word1[0].nBoot0 = set;
+    m_data.word1[1].nBoot0 = !set;
 }
 
 void OptionBytes::setNBoot1(bool set)
 {
-    setBit(nBOOT1_ADDR, nBOOT1_POS, set);
+    m_data.word1[0].nBoot1 = set;
+    m_data.word1[1].nBoot1 = !set;
 }
 
 void OptionBytes::setNSwBoot0(bool set)
 {
-    setBit(nSWBOOT0_ADDR, nSWBOOT0_POS, set);
+    m_data.word1[0].nSwBoot0 = set;
+    m_data.word1[1].nSwBoot0 = !set;
 }
 
-bool OptionBytes::getBit(size_t addr, size_t pos) const
+void OptionBytes::setBORLEV(uint8_t val)
 {
-    const uint idx = addr + pos / 8;
-    const uint cidx = idx + sizeof(uint32_t);
-    const char mask = 1U << (pos % 8);
-
-    const auto bit = (m_data[idx] & mask) != 0;
-    const auto cbit = (m_data[cidx] & mask) != 0;
-
-    check_continue(bit == !cbit, "Option bytes complement mismatch");
-    return bit;
-}
-
-void OptionBytes::setBit(size_t addr, size_t pos, bool set)
-{
-    const uint idx = addr + pos / 8;
-    const uint cidx = idx + sizeof(uint32_t);
-    const char mask = 1U << (pos % 8);
-
-    char byte, cbyte;
-
-    if(set) {
-        byte = m_data[idx] | mask;
-        cbyte = m_data[cidx] & (~mask);
-    } else {
-        byte = m_data[idx] & (~mask);
-        cbyte = m_data[cidx] | mask;
-    }
-
-    m_data[idx] = byte;
-    m_data[cidx] = cbyte;
-}
-
-uint8_t OptionBytes::getByte(size_t addr, size_t pos) const
-{
-    const uint idx = addr + pos;
-    const uint cidx = idx + sizeof(uint32_t);
-
-    const uint8_t byte = m_data[idx];
-    const uint8_t cbyte = m_data[cidx];
-
-    check_continue((byte | cbyte) == 0xFF, "Option bytes mismatch");
-    return byte;
+    m_data.word1[0].nSwBoot0 = val;
+    m_data.word1[1].nSwBoot0 = ~val;
 }
 
 }
--- dfu/device/stm32wb55/optionbytes.h
+++ dfu/device/stm32wb55/optionbytes.h
@@ -8,6 +8,108 @@ namespace WB55 {
 
 class OptionBytes
 {
+
+#pragma pack(push,1)
+    struct OptionBytesData {
+        struct Word1 {
+            const unsigned RDP:8;
+            const unsigned ESE:1;
+            unsigned BORLEV:3;
+            unsigned nRSTSTOP:1;
+            unsigned nRSTSTDBY:1;
+            unsigned nRSTSHDW:1;
+            const unsigned UNUSED1:1;
+            unsigned IWDGSW:1;
+            unsigned IWDGSTOP:1;
+            unsigned IWGDSTDBY:1;
+            unsigned WWDGSW:1;
+            const unsigned UNUSED2:3;
+            unsigned nBoot1:1;
+            unsigned SRAM2PE:1;
+            unsigned SRAM2RST:1;
+            unsigned nSwBoot0:1;
+            unsigned nBoot0:1;
+            const unsigned UNUSED3:1;
+            unsigned AGCTRIM:3;
+        };
+
+        struct Word2 {
+            const unsigned PCROP1ASTRT:9;
+            const unsigned UNUSED:23;
+        };
+
+        struct Word3 {
+            const unsigned PCROP1AEND:9;
+            const unsigned UNUSED:22;
+            const unsigned PCROPRDP:1;
+        };
+
+        struct Word4 {
+            const uint8_t WRP1ASTRT;
+            const uint8_t UNUSED1;
+            const uint8_t WRP1AEND;
+            const uint8_t UNUSED2;
+        };
+
+        struct Word5 {
+            const uint8_t WRP1BSTRT;
+            const uint8_t UNUSED1;
+            const uint8_t WRP1BEND;
+            const uint8_t UNUSED2;
+        };
+
+        struct Word6 {
+            const unsigned PCROP1BSTRT:9;
+            const unsigned UNUSED:23;
+        };
+
+        struct Word7 {
+            const unsigned PCROP1BEND:9;
+            const unsigned UNUSED:23;
+        };
+
+        struct Word8 {
+            unsigned IPCCDBA:14;
+            const unsigned UNUSED:18;
+        };
+
+        struct Word9 {
+            const unsigned SFSA:8;
+            const unsigned FSD:1;
+            const unsigned UNUSED1:3;
+            const unsigned DDS:1;
+            const unsigned UNUSED2:19;
+        };
+
+        struct Word10 {
+            const unsigned SBRV:18;
+            const unsigned SBRSA:5;
+            const unsigned BRSD:1;
+            const unsigned UNUSED1:1;
+            const unsigned SNBRSA:5;
+            const unsigned NBRSD:1;
+            const unsigned C2OPT:1;
+        };
+
+        Word1 word1[2];
+        Word2 word2[2];
+        Word3 word3[2];
+        Word4 word4[2];
+        Word5 word5[2];
+        Word6 word6[2];
+        Word7 word7[2];
+
+        const uint32_t UNUSED[12];
+
+        Word8 word8[2];
+        Word9 word9[2];
+        Word10 word10[2];
+    };
+
+    static_assert (sizeof(OptionBytesData) == 128, "Check your struct packing!");
+
+#pragma pack(pop)
+
     OptionBytes();
 
 public:
@@ -16,27 +118,79 @@ public:
     static OptionBytes invalid();
     static qint64 size();
 
-    const QByteArray &data() const;
     bool isValid() const;
+    QByteArray data() const;
+
+    uint8_t RDP() const;
+
+    bool ESE() const;
+
+    uint8_t BORLEV() const;
+
+    bool nRSTSTOP() const;
+    bool nRSTSTDBY() const;
+    bool nRSTSHDW() const;
+
+    bool IWDGSW() const;
+    bool IWDGSTOP() const;
+    bool IWGDSTDBY() const;
+    bool WWDGSW() const;
 
     bool nBoot0() const;
     bool nBoot1() const;
     bool nSwBoot0() const;
 
+    bool SRAM2PE() const;
+    bool SRAM2RST() const;
+
+    uint8_t AGCTRIM() const;
+
+    bool FSD() const;
+    bool DDS() const;
+    bool BRSD() const;
+    bool NBRSD() const;
+    bool C2OPT() const;
+    bool PCROPRDP() const;
+
     uint8_t SFSA() const;
+
+    uint8_t SBRSA() const;
+    uint8_t SNBRSA() const;
+
+    uint16_t PCROP1ASTRT() const;
+    uint16_t PCROP1AEND() const;
+
+    uint16_t PCROP1BSTRT() const;
+    uint16_t PCROP1BEND() const;
+
+    uint8_t WRP1ASTRT() const;
+    uint8_t WRP1AEND() const;
+    uint8_t WRP1BSTRT() const;
+    uint8_t WRP1BEND() const;
+
+    uint16_t IPCCDBA() const;
+    uint32_t SBRV() const;
 
     void setNBoot0(bool set);
     void setNBoot1(bool set);
     void setNSwBoot0(bool set);
 
+    void setSRAM2RST(bool set);
+    void setSRAM2PE(bool set);
+
+    void setBORLEV(uint8_t val);
+
+    void setnRSTSTOP(bool set);
+    void setnRSTSTDBY(bool set);
+    void setnRSTSHDW(bool set);
+    void setIWDGSW(bool set);
+    void setIWDGSTOP(bool set);
+    void setIWGDSTDBY(bool set);
+    void setWWDGSW(bool set);
+
 private:
-    bool getBit(size_t addr, size_t pos) const;
-    void setBit(size_t addr, size_t pos, bool set);
-
-    uint8_t getByte(size_t addr, size_t pos) const;
-
-    QByteArray m_data;
     bool m_isValid;
+    OptionBytesData m_data;
 };
 
 }
