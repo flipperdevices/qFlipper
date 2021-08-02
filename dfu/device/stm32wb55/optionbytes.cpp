6881a0e72f33d8e461a3dc80cf1be845ebf5ae4f
Georgii Surkov <georgii.surkov@outlook.com>
2021-08-02 14:54:35 +0300

    Add missing OptionBytes setters

--- dfu/device/stm32wb55/optionbytes.cpp
+++ dfu/device/stm32wb55/optionbytes.cpp
@@ -3,6 +3,9 @@
 
 #define OPTION_BYTES_SIZE 128
 
+#define NORMAL 0
+#define COMPLEMENT 1
+
 namespace STM32 {
 namespace WB55 {
 
@@ -37,201 +40,243 @@ QByteArray OptionBytes::data() const
 
 bool OptionBytes::nBoot0() const
 {
-    return m_data.word1[0].nBoot0;
+    return m_data.word1[NORMAL].nBoot0;
 }
 
 bool OptionBytes::nBoot1() const
 {
-    return m_data.word1[0].nBoot1;
+    return m_data.word1[NORMAL].nBoot1;
 }
 
 bool OptionBytes::nSwBoot0() const
 {
-    return m_data.word1[0].nSwBoot0;
+    return m_data.word1[NORMAL].nSwBoot0;
 }
 
 bool OptionBytes::SRAM2PE() const
 {
-    return m_data.word1[0].SRAM2PE;
+    return m_data.word1[NORMAL].SRAM2PE;
 }
 
 bool OptionBytes::SRAM2RST() const
 {
-    return m_data.word1[0].SRAM2RST;
+    return m_data.word1[NORMAL].SRAM2RST;
 }
 
 uint8_t OptionBytes::AGCTRIM() const
 {
-    return m_data.word1[0].AGCTRIM;
+    return m_data.word1[NORMAL].AGCTRIM;
 }
 
 bool OptionBytes::FSD() const
 {
-    return m_data.word9[0].FSD;
+    return m_data.word9[NORMAL].FSD;
 }
 
 bool OptionBytes::DDS() const
 {
-    return m_data.word9[0].FSD;
+    return m_data.word9[NORMAL].FSD;
 }
 
 bool OptionBytes::BRSD() const
 {
-    return m_data.word10[0].BRSD;
+    return m_data.word10[NORMAL].BRSD;
 }
 
 bool OptionBytes::NBRSD() const
 {
-    return m_data.word10[0].NBRSD;
+    return m_data.word10[NORMAL].NBRSD;
 }
 
 bool OptionBytes::C2OPT() const
 {
-    return m_data.word10[0].C2OPT;
+    return m_data.word10[NORMAL].C2OPT;
 }
 
 bool OptionBytes::PCROPRDP() const
 {
-    return m_data.word3[0].PCROPRDP;
+    return m_data.word3[NORMAL].PCROPRDP;
 }
 
 uint8_t OptionBytes::RDP() const
 {
-    return m_data.word1[0].RDP;
+    return m_data.word1[NORMAL].RDP;
 }
 
 bool OptionBytes::ESE() const
 {
-    return m_data.word1[0].ESE;
+    return m_data.word1[NORMAL].ESE;
 }
 
 uint8_t OptionBytes::BORLEV() const
 {
-    return m_data.word1[0].BORLEV;
+    return m_data.word1[NORMAL].BORLEV;
 }
 
 bool OptionBytes::nRSTSTOP() const
 {
-    return m_data.word1[0].nRSTSTOP;
+    return m_data.word1[NORMAL].nRSTSTOP;
 }
 
 bool OptionBytes::nRSTSTDBY() const
 {
-    return m_data.word1[0].nRSTSTDBY;
+    return m_data.word1[NORMAL].nRSTSTDBY;
 }
 
 bool OptionBytes::nRSTSHDW() const
 {
-    return m_data.word1[0].nRSTSHDW;
+    return m_data.word1[NORMAL].nRSTSHDW;
 }
 
 bool OptionBytes::IWDGSW() const
 {
-    return m_data.word1[0].IWDGSW;
+    return m_data.word1[NORMAL].IWDGSW;
 }
 
 bool OptionBytes::IWDGSTOP() const
 {
-    return m_data.word1[0].IWDGSTOP;
+    return m_data.word1[NORMAL].IWDGSTOP;
 }
 
 bool OptionBytes::IWGDSTDBY() const
 {
-    return m_data.word1[0].IWGDSTDBY;
+    return m_data.word1[NORMAL].IWGDSTDBY;
 }
 
 bool OptionBytes::WWDGSW() const
 {
-    return m_data.word1[0].WWDGSW;
+    return m_data.word1[NORMAL].WWDGSW;
 }
 
 uint8_t OptionBytes::SFSA() const
 {
-    return m_data.word9[0].SFSA;
+    return m_data.word9[NORMAL].SFSA;
 }
 
 uint8_t OptionBytes::SBRSA() const
 {
-    return m_data.word10[0].SBRSA;
+    return m_data.word10[NORMAL].SBRSA;
 }
 
 uint8_t OptionBytes::SNBRSA() const
 {
-    return m_data.word10[0].SNBRSA;
+    return m_data.word10[NORMAL].SNBRSA;
 }
 
 uint16_t OptionBytes::PCROP1ASTRT() const
 {
-    return m_data.word2[0].PCROP1ASTRT;
+    return m_data.word2[NORMAL].PCROP1ASTRT;
 }
 
 uint16_t OptionBytes::PCROP1AEND() const
 {
-    return m_data.word3[0].PCROP1AEND;
+    return m_data.word3[NORMAL].PCROP1AEND;
 }
 
 uint16_t OptionBytes::PCROP1BSTRT() const
 {
-    return m_data.word6[0].PCROP1BSTRT;
+    return m_data.word6[NORMAL].PCROP1BSTRT;
 }
 
 uint16_t OptionBytes::PCROP1BEND() const
 {
-    return m_data.word7[0].PCROP1BEND;
+    return m_data.word7[NORMAL].PCROP1BEND;
 }
 
 uint8_t OptionBytes::WRP1ASTRT() const
 {
-    return m_data.word4[0].WRP1ASTRT;
+    return m_data.word4[NORMAL].WRP1ASTRT;
 }
 
 uint8_t OptionBytes::WRP1AEND() const
 {
-    return m_data.word4[0].WRP1AEND;
+    return m_data.word4[NORMAL].WRP1AEND;
 }
 
 uint8_t OptionBytes::WRP1BSTRT() const
 {
-    return m_data.word5[0].WRP1BSTRT;
+    return m_data.word5[NORMAL].WRP1BSTRT;
 }
 
 uint8_t OptionBytes::WRP1BEND() const
 {
-    return m_data.word5[0].WRP1BEND;
+    return m_data.word5[NORMAL].WRP1BEND;
 }
 
 uint16_t OptionBytes::IPCCDBA() const
 {
-    return m_data.word8[0].IPCCDBA;
+    return m_data.word8[NORMAL].IPCCDBA;
 }
 
 uint32_t OptionBytes::SBRV() const
 {
-    return m_data.word10[0].SBRV;
+    return m_data.word10[NORMAL].SBRV;
 }
 
 void OptionBytes::setNBoot0(bool set)
 {
-    m_data.word1[0].nBoot0 = set;
-    m_data.word1[1].nBoot0 = !set;
+    m_data.word1[NORMAL].nBoot0 = set;
+    m_data.word1[COMPLEMENT].nBoot0 = !set;
 }
 
 void OptionBytes::setNBoot1(bool set)
 {
-    m_data.word1[0].nBoot1 = set;
-    m_data.word1[1].nBoot1 = !set;
+    m_data.word1[NORMAL].nBoot1 = set;
+    m_data.word1[COMPLEMENT].nBoot1 = !set;
 }
 
 void OptionBytes::setNSwBoot0(bool set)
 {
-    m_data.word1[0].nSwBoot0 = set;
-    m_data.word1[1].nSwBoot0 = !set;
+    m_data.word1[NORMAL].nSwBoot0 = set;
+    m_data.word1[COMPLEMENT].nSwBoot0 = !set;
 }
 
 void OptionBytes::setBORLEV(uint8_t val)
 {
-    m_data.word1[0].nSwBoot0 = val;
-    m_data.word1[1].nSwBoot0 = ~val;
+    m_data.word1[NORMAL].nSwBoot0 = val;
+    m_data.word1[COMPLEMENT].nSwBoot0 = ~val;
+}
+
+void OptionBytes::setnRSTSTOP(bool set)
+{
+    m_data.word1[NORMAL].nRSTSTOP = set;
+    m_data.word1[COMPLEMENT].nRSTSTOP = !set;
+}
+
+void OptionBytes::setnRSTSTDBY(bool set)
+{
+    m_data.word1[NORMAL].nRSTSTDBY = set;
+    m_data.word1[COMPLEMENT].nRSTSTDBY = !set;
+}
+
+void OptionBytes::setnRSTSHDW(bool set)
+{
+    m_data.word1[NORMAL].nRSTSHDW = set;
+    m_data.word1[COMPLEMENT].nRSTSHDW = !set;
+}
+
+void OptionBytes::setIWDGSW(bool set)
+{
+    m_data.word1[NORMAL].IWDGSW = set;
+    m_data.word1[COMPLEMENT].IWDGSW = !set;
+}
+
+void OptionBytes::setIWDGSTOP(bool set)
+{
+    m_data.word1[NORMAL].IWDGSTOP = set;
+    m_data.word1[COMPLEMENT].IWDGSTOP = !set;
+}
+
+void OptionBytes::setIWGDSTDBY(bool set)
+{
+    m_data.word1[NORMAL].IWGDSTDBY = set;
+    m_data.word1[COMPLEMENT].IWGDSTDBY = !set;
+}
+
+void OptionBytes::setWWDGSW(bool set)
+{
+    m_data.word1[NORMAL].WWDGSW = set;
+    m_data.word1[COMPLEMENT].WWDGSW = !set;
 }
 
 }
