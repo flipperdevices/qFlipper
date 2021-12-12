295bd2d3b4067d60ed5253dd6e15eee473c28843
Georgii Surkov <georgii.surkov@outlook.com>
2021-12-12 21:02:59 +0300

    Add missing storage messages

--- backend/flipperzero/protobuf/storageprotobufmessage.cpp
+++ backend/flipperzero/protobuf/storageprotobufmessage.cpp
@@ -24,11 +24,11 @@ quint64 StorageInfoResponse::sizeTotal() const
     return pbMessage()->content.storage_info_response.total_space;
 }
 
-StorageStatRequest::StorageStatRequest(QSerialPort *serialPort, const QByteArray &fileName):
+StorageStatRequest::StorageStatRequest(QSerialPort *serialPort, const QByteArray &path):
     AbstractMainProtobufRequest(serialPort),
-    m_fileName(fileName)
+    m_path(path)
 {
-   pbMessage()->content.storage_stat_request.path = m_fileName.data();
+   pbMessage()->content.storage_stat_request.path = m_path.data();
 }
 
 StorageStatResponse::StorageStatResponse(QSerialPort *serialPort):
@@ -68,4 +68,58 @@ QVector<PB_Storage_File> StorageListResponse::files() const
     }
 
     return ret;
+}
+
+StorageMkdirRequest::StorageMkdirRequest(QSerialPort *serialPort, const QByteArray &path):
+    AbstractMainProtobufRequest(serialPort),
+    m_path(path)
+{
+    pbMessage()->content.storage_mkdir_request.path = m_path.data();
+}
+
+StorageRemoveRequest::StorageRemoveRequest(QSerialPort *serialPort, const QByteArray &path, bool recursive):
+    AbstractMainProtobufRequest(serialPort),
+    m_path(path)
+{
+    pbMessage()->content.storage_delete_request.path = m_path.data();
+    pbMessage()->content.storage_delete_request.recursive = recursive;
+}
+
+StorageReadRequest::StorageReadRequest(QSerialPort *serialPort, const QByteArray &path):
+    AbstractMainProtobufRequest(serialPort),
+    m_path(path)
+{
+    pbMessage()->content.storage_read_request.path = m_path.data();
+}
+
+StorageReadResponse::StorageReadResponse(QSerialPort *serialPort):
+    AbstractMainProtobufResponse(serialPort)
+{}
+
+bool StorageReadResponse::isPresent() const
+{
+    return pbMessage()->content.storage_read_response.has_file;
+}
+
+const QByteArray StorageReadResponse::data() const
+{
+    const auto &file = pbMessage()->content.storage_read_response.file;
+    return QByteArray((const char*)file.data, file.size);
+}
+
+StorageWriteRequest::StorageWriteRequest(QSerialPort *serialPort, const QByteArray &path, const QByteArray &buf):
+    AbstractMainProtobufRequest(serialPort),
+    m_path(path)
+{
+    auto &request = pbMessage()->content.storage_write_request;
+    request.path = m_path.data();
+    request.file.data = (pb_bytes_array_t*)malloc(PB_BYTES_ARRAY_T_ALLOCSIZE(buf.size()));
+    request.file.data->size = buf.size();
+    memcpy(request.file.data->bytes, buf.data(), buf.size());
+}
+
+StorageWriteRequest::~StorageWriteRequest()
+{
+    auto &request = pbMessage()->content.storage_write_request;
+    free(request.file.data);
 }
--- backend/flipperzero/protobuf/storageprotobufmessage.h
+++ backend/flipperzero/protobuf/storageprotobufmessage.h
@@ -29,10 +29,10 @@ class StorageStatRequest:
 public AbstractMainProtobufRequest<PB_Main_storage_stat_request_tag>
 {
 public:
-    StorageStatRequest(QSerialPort *serialPort, const QByteArray &fileName);
+    StorageStatRequest(QSerialPort *serialPort, const QByteArray &path);
 
 private:
-    QByteArray m_fileName;
+    QByteArray m_path;
 };
 
 class StorageStatResponse:
@@ -61,6 +61,57 @@ public AbstractMainProtobufResponse<PB_Main_storage_list_response_tag>
 public:
     StorageListResponse(QSerialPort *serialPort);
     QVector<PB_Storage_File> files() const;
+};
+
+class StorageMkdirRequest:
+public AbstractMainProtobufRequest<PB_Main_storage_mkdir_request_tag>
+{
+public:
+    StorageMkdirRequest(QSerialPort *serialPort, const QByteArray &path);
+
+private:
+    QByteArray m_path;
+};
+
+class StorageRemoveRequest:
+public AbstractMainProtobufRequest<PB_Main_storage_delete_request_tag>
+{
+public:
+    StorageRemoveRequest(QSerialPort *serialPort, const QByteArray &path, bool recursive);
+
+private:
+    QByteArray m_path;
+};
+
+class StorageReadRequest:
+public AbstractMainProtobufRequest<PB_Main_storage_read_request_tag>
+{
+public:
+    StorageReadRequest(QSerialPort *serialPort, const QByteArray &path);
+
+private:
+    QByteArray m_path;
+};
+
+class StorageReadResponse:
+public AbstractMainProtobufResponse<PB_Main_storage_read_response_tag>
+{
+public:
+    StorageReadResponse(QSerialPort *serialPort);
+
+    bool isPresent() const;
+    const QByteArray data() const;
+};
+
+class StorageWriteRequest:
+public AbstractMainProtobufRequest<PB_Main_storage_write_request_tag>
+{
+public:
+    StorageWriteRequest(QSerialPort *serialPort, const QByteArray &path, const QByteArray &buf);
+    ~StorageWriteRequest();
+
+private:
+    QByteArray m_path;
 };
 
 }
