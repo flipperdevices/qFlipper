300692e1fbd83547f7f965bedd25a115bf76909f
Georgii Surkov <georgii.surkov@outlook.com>
2021-12-31 17:00:15 +0300

    Fix a typo

--- backend/applicationbackend.h
+++ backend/applicationbackend.h
@@ -35,7 +35,7 @@ public:
         Press, /* Press event, emitted after debounce */
         Release, /* Release event, emitted after debounce */
         Short, /* Short event, emitted after InputTypeRelease done withing INPUT_LONG_PRESS interval */
-        Long, /* Long event, emmited after INPUT_LONG_PRESS interval, asynchronouse to InputTypeRelease  */
+        Long, /* Long event, emmited after INPUT_LONG_PRESS interval, asynchronous to InputTypeRelease  */
         Repeat, /* Repeat event, emmited with INPUT_REPEATE_PRESS period after InputTypeLong event */
     };
 
