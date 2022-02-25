e48fa2807504c08a42803e3b75d54cb227e8fbf5
Georgii Surkov <georgii.surkov@outlook.com>
2022-02-25 12:35:47 +0300

    Bundle libusb within AppImage

--- build_linux.sh
+++ build_linux.sh
@@ -5,10 +5,11 @@ set -ex
 TARGET='qFlipper'
 BUILDDIR='build'
 APPDIR_PREFIX="$PWD/$BUILDDIR/AppDir/usr"
+LIBUSB_OVERRIDE="$(ldconfig -p | grep x86-64 | grep -oP '/[^\s]+/libusb-1.0\.so\.\d+$' | head -n1)"
 
 export OUTPUT=$TARGET-x86_64.AppImage
 export QML_SOURCES_PATHS='..'
 
 mkdir -p $BUILDDIR && cd $BUILDDIR
 qmake ../$TARGET.pro -spec linux-g++ CONFIG+=qtquickcompiler PREFIX=$APPDIR_PREFIX && make qmake_all && make -j$(nproc) && make install
-linuxdeploy --appdir=AppDir -o appimage --custom-apprun=../installer-assets/appimage/AppRun --plugin=qt
+linuxdeploy --appdir=AppDir -o appimage --custom-apprun=../installer-assets/appimage/AppRun --library=$LIBUSB_OVERRIDE --plugin=qt 
