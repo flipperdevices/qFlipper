26c431ffb929327393029f676d3d9c58f431b0bd
Deven Blake <github@trinity.moe>
2022-03-10 22:48:38 -0500

    Fix accidental SC1004 error

--- setup_rules.sh
+++ setup_rules.sh
@@ -3,8 +3,7 @@ set -e
 
 RULES_DIR=/etc/udev/rules.d
 RULES_FILE="$RULES_DIR"/42-flipperzero.rules
-RULES_TEXT='\
-#Flipper Zero serial port
+RULES_TEXT='#Flipper Zero serial port
 SUBSYSTEMS=="usb", ATTRS{idVendor}=="0483", ATTRS{idProduct}=="5740", ATTRS{manufacturer}=="Flipper Devices Inc.", TAG+="uaccess"
 #Flipper Zero DFU
 SUBSYSTEMS=="usb", ATTRS{idVendor}=="0483", ATTRS{idProduct}=="df11", ATTRS{manufacturer}=="STMicroelectronics", TAG+="uaccess"
