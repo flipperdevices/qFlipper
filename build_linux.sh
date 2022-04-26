ac3476828460d82a8303772e95c828ce7eadcc5c
Georgii Surkov <georgii.surkov@outlook.com>
2022-04-26 17:22:04 +0300

    Include libssl1.1 into AppImage build

--- build_linux.sh
+++ build_linux.sh
@@ -5,11 +5,11 @@ set -ex
 TARGET='qFlipper'
 BUILDDIR='build'
 APPDIR_PREFIX="$PWD/$BUILDDIR/AppDir/usr"
-#LIBUSB_OVERRIDE="$(ldconfig -p | grep x86-64 | grep -oP '/[^\s]+/libusb-1.0\.so\.\d+$' | head -n1)"
+LIBSSL1_OVERRIDE="$(ldconfig -p | grep x86-64 | grep -oP '/[^\s]+/libssl.so.1.1' | head -n1)"
 
 export OUTPUT=$TARGET-x86_64.AppImage
 export QML_SOURCES_PATHS='..'
 
 mkdir -p $BUILDDIR && cd $BUILDDIR
 qmake ../$TARGET.pro -spec linux-g++ CONFIG+=qtquickcompiler PREFIX=$APPDIR_PREFIX && make qmake_all && make -j$(nproc) && make install
-linuxdeploy --appdir=AppDir -o appimage --custom-apprun=../installer-assets/appimage/AppRun --plugin=qt 
+linuxdeploy --appdir=AppDir -o appimage --custom-apprun=../installer-assets/appimage/AppRun --plugin=qt --library=$LIBSSL1_OVERRIDE
