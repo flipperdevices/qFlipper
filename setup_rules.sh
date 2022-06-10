c6bf19afd6793f568880704d08df344141bf70df
Ivan <vanish125@users.noreply.github.com>
2022-06-10 16:32:09 +0300

    Update setup_rules.sh
    Add rules for ESP32s2 BlackMagic

--- setup_rules.sh
+++ setup_rules.sh
@@ -7,6 +7,8 @@ RULES_TEXT='#Flipper Zero serial port
 SUBSYSTEMS=="usb", ATTRS{idVendor}=="0483", ATTRS{idProduct}=="5740", ATTRS{manufacturer}=="Flipper Devices Inc.", TAG+="uaccess"
 #Flipper Zero DFU
 SUBSYSTEMS=="usb", ATTRS{idVendor}=="0483", ATTRS{idProduct}=="df11", ATTRS{manufacturer}=="STMicroelectronics", TAG+="uaccess"
+#Flipper ESP32s2 BlackMagic
+SUBSYSTEMS=="usb", ATTRS{idVendor}=="303a", ATTRS{idProduct}=="40??", ATTRS{manufacturer}=="Flipper Devices Inc.", TAG+="uaccess"
 '
 
 alias warning_message='printf "You will now be asked for SUDO password.\n"'
