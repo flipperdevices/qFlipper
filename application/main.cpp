701ffe5625a3152c9befe452368a61b828c1ba44
Georgii Surkov <georgii.surkov@outlook.com>
2023-03-03 22:16:59 +0300

    Improve DPI scaling behaviour

--- application/main.cpp
+++ application/main.cpp
@@ -10,9 +10,12 @@ int main(int argc, char *argv[])
     QCoreApplication::setApplicationVersion(APP_VERSION);
     QCoreApplication::setOrganizationName(QStringLiteral("Flipper Devices Inc"));
     QCoreApplication::setOrganizationDomain(QStringLiteral("flipperdevices.com"));
+
 #if QT_VERSION < 0x060000
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 #endif
+
+    QGuiApplication::setHighDpiScaleFactorRoundingPolicy(Qt::HighDpiScaleFactorRoundingPolicy::RoundPreferFloor);
 
     Application app(argc, argv);
     return app.exec();
