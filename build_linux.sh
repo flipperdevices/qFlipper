5dcd79a1f3a73e87de6d947e174a8423885d8585
Georgii Surkov <georgii.surkov@outlook.com>
2023-03-23 17:36:41 +0300

    Exclude unnecessary X11 and wayland libraries

--- build_linux.sh
+++ build_linux.sh
@@ -5,14 +5,27 @@ set -euxo pipefail;
 TARGET="qFlipper"
 BUILDDIR="build"
 APPDIR_PREFIX="$PWD/$BUILDDIR/AppDir/usr"
+
 LIBSSL1_OVERRIDE="$(ldconfig -p | grep x86-64 | grep -oP '/[^\s]+/libssl.so.1.1' | head -n1)"
+
+LIBWAYLAND_EXCLUDE="libwayland*"
+LIBXCB_EXCLUDE="libxcb*"
+LIBXKB_EXCLUDE="libxkb*"
+LIBX11_EXCLUDE="libX*"
 
 export OUTPUT="$TARGET-x86_64.AppImage"
 
-mkdir -p "$BUILDDIR"
-cd "$BUILDDIR"
-qmake "../$TARGET.pro" -spec linux-g++ CONFIG+=qtquickcompiler PREFIX="$APPDIR_PREFIX"
+mkdir -p "$BUILDDIR" && cd "$BUILDDIR"
+
+qmake "../$TARGET.pro" -spec linux-g++ "CONFIG+=release qtquickcompiler" PREFIX="$APPDIR_PREFIX"
 make qmake_all
 make -j"$(nproc)"
 make install
-linuxdeploy --appdir=AppDir -o appimage --custom-apprun="../installer-assets/appimage/AppRun"  --library="$LIBSSL1_OVERRIDE"
+
+linuxdeploy --appdir=AppDir -o appimage \
+    --custom-apprun="../installer-assets/appimage/AppRun" \
+    --library="$LIBSSL1_OVERRIDE" \
+    --exclude-library="$LIBWAYLAND_EXCLUDE" \
+    --exclude-library="$LIBXCB_EXCLUDE" \
+    --exclude-library="$LIBXKB_EXCLUDE" \
+    --exclude-library="$LIBX11_EXCLUDE"
